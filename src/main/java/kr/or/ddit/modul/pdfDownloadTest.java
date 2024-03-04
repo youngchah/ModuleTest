fa0455ac34a858473d4f369bfe6a446a15507590
Young chah <toast0801@gmail.com>
2024-03-04 21:54:53 +0900

    24.03.04 모듈테이스 커밋

---
+++ src/main/java/kr/or/ddit/modul/pdfDownloadTest.java
@@ -0,0 +1,29 @@
+package kr.or.ddit.modul;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/pdf")
+@Slf4j
+public class pdfDownloadTest {
+	
+	@RequestMapping(value = "/pdfDownload", method = RequestMethod.GET)
+	public String pdfDownload(Model model) {
+		log.info("pdfDownload() 실행..!");
+		
+		return "pdf/pdfDownload";
+	}
+	@RequestMapping(value = "/pdfMain", method = RequestMethod.GET)
+	public String pdfMain(Model model) {
+		log.info("pdfMain() 실행..!");
+		
+		return "pdf/pdfMain";
+	}
+	
+	
+}
